228472f81669c4a367f6658dbbe60c1ca7529801
mkgood20 <60886177+mkgood20@users.noreply.github.com>
2020-02-13 10:36:22 -0500

    Add files via upload
    initial file

---
+++ secrethangman.py
@@ -0,0 +1,36 @@
+import random
+randomWords = ["ducks" , "jumbo" ,"lucky" , "pills" , "flour"]
+secret = random.choice (randomWords)
+letter = ""
+updateWord = []
+#print secret 
+def initialize(): 
+    print "We have a secret word"
+    print "_ _ _ _ _"
+def getLetter():
+    print "Enter a letter"
+    global letter
+    letter = raw_input()
+def ifWon():
+    if secret == updateWord: 
+        print "you win"
+    else:
+        getLetter()
+def main():
+    initialize ()
+    getLetter()
+    ifWon()
+main()
+
+#def tester():
+    #global updateWord
+    #updateWord = raw_input()
+    #if updateWord== secret:
+     #   print "yay"
+    #else: 
+     #   print "no"
+  
+    
+    
+    
+
