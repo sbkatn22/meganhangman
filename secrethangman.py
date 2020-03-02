3904de7d083f78706c1d7d7f7fd7d7b957e7eabf
sbkatn22 <60975446+sbkatn22@users.noreply.github.com>
2020-03-02 10:05:33 -0500

    Update secrethangman.py

--- secrethangman.py
+++ secrethangman.py
@@ -1,24 +1,25 @@
 import random
 randomWords = ["ducks" , "jumbo" ,"lucky" , "pills" , "flour"]
-secret = random.choice (randomWords)
+secret = random.choice(randomWords)
 letter = ""
 updateWord = []
 #print secret 
 def initialize(): 
-    print "We have a secret word"
-    print "_ _ _ _ _"
+    print ("We have a secret word")
+    print ("_ _ _ _ _")
 def getLetter():
-    print "Enter a letter"
+    print ("Enter a letter")
     global letter
     letter = raw_input()
 def ifWon():
     if secret == updateWord: 
-        print "you win"
+        print ("you win")
     else:
         getLetter()
 def main():
     initialize ()
     getLetter()
+    test(letter)
     ifWon()
 main()
 
@@ -30,7 +31,15 @@ main()
     #else: 
      #   print "no"
   
-    
+def test(letter):
+    global updatedWord
+    global letter
+    global secret
+    if letter in secret:
+        updatedWord.append(letter)
+        for i in range(0, len(secret)):
+            print (updatedWord[i]),
+        ifWon()   
     
     
 
